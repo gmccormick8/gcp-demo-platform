e46d0a05aa8539610db039dd34e7b77ec18d64b7
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-06-10 09:37:33 -0700

    Rename IAM service account resource from "test" to "testing" for clarity and consistency.

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_iam_service_account" "test" {
+resource "google_iam_service_account" "testing" {
   account_id   = "demo-account"
   display_name = "demo"
   project      = var.project_id

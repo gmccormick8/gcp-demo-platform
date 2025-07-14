87af34c770ded083762bb8a1f75c9951945c3b89
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-07-14 12:49:21 -0700

    fix: Update ArgoCD output syntax for load balancer ingress IP retrieval

--- modules/argocd/outputs.tf
+++ modules/argocd/outputs.tf
@@ -1,4 +1,4 @@
 output "argocd_endpoint" {
   description = "The endpoint of the ArgoCD server"
-  value       = helm_release.argocd.status["load_balancer_ingress"][0]["ip"]
+  value       = helm_release.argocd.status.load_balancer_ingress[0].ip
 }

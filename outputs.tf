d4e26ded66a11c1900270fe5c49125887abba01a
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-07-14 13:33:12 -0700

    fix: Remove ArgoCD endpoints output from outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -11,12 +11,3 @@ output "gke_cluster_endpoints" {
     for key, cluster in module.gke_clusters : key => cluster.cluster_endpoint
   }
 }
-
-output "argocd_endpoints" {
-  description = "Endpoints for ArgoCD servers in each cluster"
-  value = {
-    central = module.argocd_central.argocd_endpoint
-    east    = module.argocd_east.argocd_endpoint
-    west    = module.argocd_west.argocd_endpoint
-  }
-}

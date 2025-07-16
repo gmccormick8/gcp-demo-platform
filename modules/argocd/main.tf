e3c5c4f0077c33ce3b53e49963ae264b0866dd38
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-07-16 11:37:00 -0700

    refactor: Enhance ArgoCD module by defining Kubernetes namespace and updating configuration parameters

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -1,7 +1,19 @@
+resource "kubernetes_namespace_v1" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
 module "argocd" {
   source = "squareops/argocd/kubernetes"
   argocd_config = {
     hostname    = "argocd.prod.in"
     values_yaml = file("${path.module}/helm/values.yaml")
+    argocd_notifications_enabled = false
+    autoscaling_enabled = false
+    ingress_class_name = "loadbalancer"
+    redis_ha_enabled = false
+    slack_notification_token = ""
   }
+  namespace = kubernetes_namespace_v1.argocd.metadata[0].name
 }

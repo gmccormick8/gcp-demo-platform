91ebaf0c76274d601c88daf908d96c344f48b746
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-07-16 12:05:52 -0700

    fix: Update Kubernetes and Helm provider host configuration to include HTTPS

--- modules/argocd/providers.tf
+++ modules/argocd/providers.tf
@@ -14,14 +14,14 @@ terraform {
 }
 
 provider "kubernetes" {
-  host                   = var.cluster_endpoint
+  host                   = "https://${var.cluster_endpoint}"
   token                  = var.access_token
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host                   = var.cluster_endpoint
+    host                   = "https://${var.cluster_endpoint}"
     token                  = var.access_token
     cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
   }

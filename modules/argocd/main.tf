4a9644fa29acd312fa7f02d51f3615fd41c10768
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-07-16 10:33:46 -0700

    refactor: Remove unused provider configurations and streamline ArgoCD module setup

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -1,11 +1,3 @@
-data "google_client_config" "current" {
-}
-
-data "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.cluster_location
-}
-
 module "argocd" {
   source = "squareops/argocd/kubernetes"
   argocd_config = {
---
+++ modules/argocd/providers.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = "~> 1.11"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.10"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.30"
+    }
+  }
+}
+
+data "google_client_config" "default" {
+}
+
+data "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.cluster_location
+}
+
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${data.google_container_cluster.primary.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  }
+}

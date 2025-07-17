14b4d0347020bc5514dd0a04a4611aff98e2fd4e
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-07-17 14:12:10 -0700

    feat: Add ApplicationSet resource for demo application in ArgoCD

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -71,3 +71,57 @@ resource "helm_release" "argocd" {
     kubernetes_service_account.argocd_k8s
   ]
 }
+
+
+# Deploy an ApplicationSet to ArgoCD for a public Git repo
+resource "kubernetes_manifest" "argocd_applicationset" {
+  manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind"       = "ApplicationSet"
+    "metadata" = {
+      "name"      = "demo-applicationset"
+      "namespace" = var.namespace
+    }
+    "spec" = {
+      "generators" = [
+        {
+          "list" = {
+            "elements" = [
+              {
+                "cluster" = "in-cluster"
+                "name"    = "demo-app"
+              }
+            ]
+          }
+        }
+      ]
+      "template" = {
+        "metadata" = {
+          "name" = "demo-app"
+        }
+        "spec" = {
+          "project" = "default"
+          "source" = {
+            "repoURL"        = "https://github.com/gmccormick8/gcp-demo-app.git"
+            "targetRevision" = "HEAD"
+            "path"           = "."
+          }
+          "destination" = {
+            "server"    = "https://kubernetes.default.svc"
+            "namespace" = var.namespace
+          }
+          "syncPolicy" = {
+            "automated" = {
+              "prune"    = true
+              "selfHeal" = true
+            }
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}

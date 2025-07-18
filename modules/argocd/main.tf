10283dd24b49e50757787bb2716069f1668dc000
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-07-18 15:33:25 -0700

    fix: Update service account ID references from email to name in IAM bindings

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -15,13 +15,19 @@ resource "kubernetes_service_account" "argocd_k8s" {
 }
 
 resource "google_service_account_iam_binding" "workload_identity_binding" {
-  service_account_id = google_service_account.argocd_gcp_sa.email
+  service_account_id = google_service_account.argocd_gcp_sa.name
   role               = "roles/iam.workloadIdentityUser"
   members            = ["serviceAccount:${var.project_id}.svc.id.goog[${var.namespace}/${kubernetes_service_account.argocd_k8s.metadata[0].name}]"]
 }
 
 resource "google_service_account_iam_binding" "cluster_admin_binding" {
-  service_account_id = google_service_account.argocd_gcp_sa.email
+  service_account_id = google_service_account.argocd_gcp_sa.name
+  role               = "roles/container.clusterUser"
+  members            = ["serviceAccount:${var.project_id}.svc.id.goog[${var.namespace}/${kubernetes_service_account.argocd_k8s.metadata[0].name}]"]
+}
+
+resource "google_service_account_iam_binding" "cluster_admin_binding" {
+  service_account_id = google_service_account.argocd_gcp_sa.name
   role               = "roles/container.clusterAdmin"
   members            = ["serviceAccount:${var.project_id}.svc.id.goog[${var.namespace}/${kubernetes_service_account.argocd_k8s.metadata[0].name}]"]
 }

c4a8a82a23d336e0e6546629a000df66f88674ac
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-07-21 11:18:26 -0700

    bug fixes

--- outputs.tf
+++ outputs.tf
@@ -4,8 +4,3 @@ output "gke_cluster_endpoints" {
     for key, cluster in module.gke_clusters : key => cluster.cluster_endpoint
   }
 }
-
-output "argocd_server_url" {
-  description = "ArgoCD URL"
-  value       = module.argocd_central.argocd_release.argocd_server_url
-}

0d1817761deb7868560a15dcff8a2e4446c94ba4
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-07-22 12:07:05 -0700

    bug fix

--- main.tf
+++ main.tf
@@ -229,7 +229,7 @@ resource "terraform_data" "fleet_membership_cleanup" {
 resource "terraform_data" "neg_cleanup" {
   triggers_replace = {
     project_id = var.project_id
-    zones       = join(" ", [for cluster in local.clusters : cluster.zone])
+    zones       = [for cluster in local.clusters : cluster.zone]
   }
 
   provisioner "local-exec" {
